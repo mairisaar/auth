e51262f3cffbddddaa6fe1225e4c3e19946a27f0
Mairi Saar <mairi.saar@khk.ee>
2023-02-02 11:21:16 +0200

    add submit secrets

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   googleId: String,
-  facebookId: String
+  facebookId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -126,16 +127,29 @@ app.get("/logout", function(req, res, next){
 });
 
 app.get("/secrets", function(req, res){
+  User.find({"secret": {$ne:null}}, function(err, foundUsers){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUsers){
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  })
+  // if(req.isAuthenticated()){
+  //   res.render("secrets");
+  // }else{
+  //   res.redirect("/login")
+  // }
+});
+
+app.get("/submit", function(req, res){
   if(req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   }else{
     res.redirect("/login")
   }
 });
-
-app.get("/submit", function(req, res){
-  res.render("submit");
-})
 
 app.post("/register", function(req, res){
 
@@ -170,6 +184,23 @@ app.post("/login", function(req, res){
   });
 });
 
+app.post("/submit", function(req, res){
+  const submitedSecret = req.body.secret;
+
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        foundUser.secret = submitedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+
+});
 
 
 app.listen(3000, function(){
